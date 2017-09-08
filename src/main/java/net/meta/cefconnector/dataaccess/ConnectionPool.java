1370023e40696fad8a8a1ed5632495f89cc9c75a
Jain, Atul <atjain@akamai.com>
2017-09-08 11:00:12 -0400

    This is nice to have fix where Readme.txt need to have externalized project version. Also Updated the version in pom to 2.0.0-SNAPSHOT Also took care of warning while creating the Hikaricp
    Former-commit-id: f1dac4631bed86f004c93f38742f07c986349d41 [formerly 05b912e29f0ae72ee604de7c87add2e36e89afbf] [formerly f1dac4631bed86f004c93f38742f07c986349d41 [formerly 05b912e29f0ae72ee604de7c87add2e36e89afbf] [formerly 05b912e29f0ae72ee604de7c87add2e36e89afbf [formerly 05b912e29f0ae72ee604de7c87add2e36e89afbf [formerly 5a8855eeb041f1cdd48eb8d15259725eb5b4ab07]]]]
    
    Former-commit-id: 6535e783741be84766f23788208d7ac3dcc8ada4
    Former-commit-id: 102bfe9c40d180b0467928b90feaa4e81272b621 [formerly 1660240aa30b814d1618d29848d7afa9761b4f21]
    Former-commit-id: bd5e09010ae2da8326e32c38bc31f43fd83e1778
    Former-commit-id: 5cc063b3e74a8d0412b564b929082a9cec7e6ea4

--- src/main/java/net/meta/cefconnector/dataaccess/ConnectionPool.java
+++ src/main/java/net/meta/cefconnector/dataaccess/ConnectionPool.java
@@ -14,6 +14,7 @@ public enum ConnectionPool {
 
 	private ConnectionPool() {
 		HikariConfig config = new HikariConfig();
+		config.setDataSourceClassName("org.sqlite.SQLiteDataSource");
 		config.setJdbcUrl("jdbc:sqlite:cefconnector.db");
 		config.setIdleTimeout(0);
 		config.setAutoCommit(true);
